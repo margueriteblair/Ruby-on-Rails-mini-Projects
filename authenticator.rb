3ebe914d6707e77a1b33a78f94c503535f8b7341
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-04 19:31:38 -0500

    setting up the bare bones for a mini auth app

---
+++ authenticator.rb
@@ -0,0 +1,12 @@
+users= [
+    {username: "Margie", password: "password1"},
+    {username: "John", password: "password5"},
+    {username: "arya", password: "password3"},
+    {username: "CareerDevs", password: "password4"}
+]
+
+puts "Welcome to the authenticator"
+25.times { print "-" }
+puts #for new line
+puts "This program will take input from the user and compare the password."
+puts "If the password is correct, you will get back the user object."
