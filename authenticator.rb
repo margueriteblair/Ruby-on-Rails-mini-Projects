8247445d97bc0dbb42fa739c38ad7cfe2953121b
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-04 19:55:26 -0500

    added other stuff

--- authenticator.rb
+++ authenticator.rb
@@ -11,12 +11,14 @@ puts #for new line
 puts "This program will take input from the user and compare the password."
 puts "If the password is correct, you will get back the user object."
 
-pw_attepmt = gets.chomp
-
-users.each do |user|
-    if user['password'] == pw_attepmt
-        puts "Yay! Logged in."
-    else 
-        puts "Oh no! You've got 2 attempts remaining."
-    end
+attempts = 1
+while attempts < 4
+    print "Username: "
+    username_attempt = gets.chomp
+    print "Password: "
+    password_attempt = gets.chomp
+    puts "Press n to quit or any other key to continue:"
+    input = gets.chomp.downcase
+    break if input == "n"
+    attempts += 1
 end
