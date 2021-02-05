e1d2ca63c7c89c8127fa15319e3f419c35616a0f
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-04 20:39:35 -0500

    wrapped up mini auth program with a message to log after the attempts are maxedout

--- authenticator.rb
+++ authenticator.rb
@@ -12,15 +12,12 @@ puts "This program will take input from the user and compare the password."
 puts "If the password is correct, you will get back the user object."
 
 def auth_user(username, password, list_of_users)
-    users.each do |user|
-        if user[:username] == username_attempt && user[:password] == password_attempt
-            puts user
-            break
-        else
-            puts "Credentials were not correct"
-            break
+    list_of_users.each do |user|
+        if user[:username] == username && user[:password] == password
+            return user
         end
     end
+    return "Credentials were not correct"
 end
 
 
@@ -30,8 +27,10 @@ while attempts < 4
     username_attempt = gets.chomp
     print "Password: "
     password_attempt = gets.chomp
+    authentication = auth_user(username_attempt, password_attempt, users)
     puts "Press n to quit or any other key to continue:"
     input = gets.chomp.downcase
     break if input == "n"
     attempts += 1
 end
+puts "You have exceeded the number of attempts" if attempts == 4
