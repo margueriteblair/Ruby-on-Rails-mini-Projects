55a3f6dab4bec25f4450d33d9c5a2225cf124f77
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-04 20:18:01 -0500

    making aseparate method for authenticating users

--- authenticator.rb
+++ authenticator.rb
@@ -11,18 +11,25 @@ puts #for new line
 puts "This program will take input from the user and compare the password."
 puts "If the password is correct, you will get back the user object."
 
+def auth_user(username, password, list_of_users)
+    users.each do |user|
+        if user[:username] == username_attempt && user[:password] == password_attempt
+            puts user
+            break
+        else
+            puts "Credentials were not correct"
+            break
+        end
+    end
+end
+
+
 attempts = 1
 while attempts < 4
     print "Username: "
     username_attempt = gets.chomp
     print "Password: "
     password_attempt = gets.chomp
-    users.each do |user|
-        if user[:username] == username_attempt && user[:password] == password_attempt
-            puts user
-        else
-            puts "Credentials were not correct"
-        end
     puts "Press n to quit or any other key to continue:"
     input = gets.chomp.downcase
     break if input == "n"
