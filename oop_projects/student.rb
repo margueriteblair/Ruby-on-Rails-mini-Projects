51bdc770e5d1f97860b9a65de1c7f4981cd25140
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-06 19:51:34 -0500

    getters by the same name

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -14,9 +14,16 @@ class Student
     def last_name=(name)
         @last_name=name
     end
+    def first_name
+        return @first_name
+    end
+    def last_name
+        return @last_name
+    end
 end
 
 margie = Student.new
 margie.first_name = "Margie"
 margie.last_name = "Blair"
 puts margie #prints out a hex representation of the class
+puts margie.first_name
