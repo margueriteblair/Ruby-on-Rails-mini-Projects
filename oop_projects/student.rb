8ed290c27ed301137bc0522c87ef84921a0cf0f1
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-06 20:36:14 -0500

    ruby accessor method lets us use both getters and setters all in one

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -1,4 +1,5 @@
 class Student
+    attr_accessor :first_name, :last_name, :email, :username
     @first_name
     @last_name
     @email
@@ -8,22 +9,13 @@ class Student
     def to_s
         "First name: #{@first_name}, last name: #{@last_name}"
     end
-    def first_name=(name)
-        @first_name=name
-    end
-    def last_name=(name)
-        @last_name=name
-    end
-    def first_name
-        return @first_name
-    end
-    def last_name
-        return @last_name
-    end
+
 end
 
 margie = Student.new
 margie.first_name = "Margie"
 margie.last_name = "Blair"
+margie.email = "bigmarg@example.com"
+margie.username = "margieb"
 puts margie #prints out a hex representation of the class
 puts margie.first_name
