234b233e468f3cb038778ed149e2ab180b4cef9d
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-07 13:06:24 -0500

    created two other methods to take the hashed password and check to see if it's valid

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -18,3 +18,14 @@ users = [
 def create_hash_digest(password)
     BCrypt::Password.create(password)
 end
+
+def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+        user_record[:password] = create_hash_digest(user_record[:password])
+    end 
+    list_of_users
+end
