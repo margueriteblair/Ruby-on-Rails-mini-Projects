17df7ff182c1df5363e2a4b7210efdc9aea63d18
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-06 19:39:07 -0500

    student rb

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -4,7 +4,13 @@ class Student
     @email
     @username
     @password
+    #the @ symbol represents the instance variable, as opposed to a normal variable
+    def to_s
+        "First name: #{@first_name}, last name: #{@last_name}"
+    end
 end
 
 margie = Student.new
+#classes have a built in to_s method that stringifies
+
 puts margie #prints out a hex representation of the class
